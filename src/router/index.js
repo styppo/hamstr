c878c9291175d814bc1a23428f43a72e27762b11
styppo <mail@styp-rekowsky.de>
2023-01-18 21:14:58 +0000

    Restore scroll position after page change

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,13 @@ export default route(function (/* { store, ssrContext } */) {
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
 
   const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        return { top: 0 }
+      }
+    },
     routes,
 
     // Leave this as is and make changes in quasar.conf.js instead!
